08101755f7a02f1540f2fb9ad990ff288ac82e27
John O'Connor <jpo424@mbp.local>
2011-11-29 21:15:29 -0500

    explicit workaround for id fetching when using sqlite

--- sql_manager/lib.py
+++ sql_manager/lib.py
@@ -23,6 +23,14 @@ def get_id(session):
     
     session - the current SQLAlchemy session
     """
-    sequence = Sequence("triple_id_seq")
-    next_id = session.connection().execute(sequence)
+    # explicit workaround for sqlite given its lack of use
+    # of sequence tables for managing ids
+    engine_text = str(session.bind.engine)
+    if 'sqlite' in engine_text:
+        next_id = session.connection().execute("select max(id) from triples_object_inferred;").first()
+        next_id = (next_id[0], 0)[next_id[0]== None] + 1
+        print next_id
+    else:
+        sequence = Sequence("triple_id_seq")
+        next_id = session.connection().execute(sequence)
     return next_id
